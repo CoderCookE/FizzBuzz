4efae97491cea6d7885c8dd371befcdf30f00fe9
CoderCookE <ejcook111@gmail.com>
2013-11-17 20:14:21 -0500

    fizzBuzz Call

---
+++ fizzBuzz.js
@@ -0,0 +1,16 @@
+var fizzBuzz = function(){
+	var n = prompt("Pick an upperbound for fizzBuzz");
+	for(var i = 1; i <= n; i++){
+		if(i%3 === 0 && i%5){
+			document.getElementById('demo').innerHTML += "fizzBuzz <br>";
+		}else if(i%3 ===0){
+			document.getElementById('demo').innerHTML += "fizz <br>";
+		}else if(i%5 ===0){
+			document.getElementById('demo').innerHTML += "Buzz <br>";
+		}else{
+			document.getElementById('demo').innerHTML += i + "<br>";
+		}
+	}
+
+
+}
