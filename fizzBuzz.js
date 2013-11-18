e56235b6a3b7d55d97914511484a77e60db4505b
CoderCookE <ejcook111@gmail.com>
2013-11-17 20:19:36 -0500

    fixed fizzBuzz check

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,7 +1,7 @@
 var fizzBuzz = function(){
 	var n = prompt("Pick an upperbound for fizzBuzz");
 	for(var i = 1; i <= n; i++){
-		if(i%3 === 0 && i%5){
+		if(i%3 === 0 && i%5=== 0){
 			document.getElementById('demo').innerHTML += "fizzBuzz <br>";
 		}else if(i%3 ===0){
 			document.getElementById('demo').innerHTML += "fizz <br>";
